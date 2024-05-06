f0d21c0b5e455abce4074d3aae9f5161fb641929
CrustyCrash <167889402+CrustyCrash@users.noreply.github.com>
2024-05-06 12:20:18 +0530

    Update Factorial.c

--- Assignment/Factorial.c
+++ Assignment/Factorial.c
@@ -17,4 +17,20 @@ int main()
         fact = fact * i;
     }
     printf("Factorial is %d \n",fact);
+
+    // Without Reduction
+
+    // int num;
+    // int fact = 1;
+    // printf("Enter a number\n");
+    // scanf("%d",&num);
+
+    // #pragma omp parallel for num_threads(4)
+    // for (int i = 2; i <= num; i++)
+    // {
+    //     #pragma omp atomic
+    //     fact = fact * i;
+    // }
+
+    // printf("%d\n", fact);
 }
